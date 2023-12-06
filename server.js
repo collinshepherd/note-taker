ddfa9d84282f9297ced3add1f5337b89f37f4d64
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2023-12-05 21:38:19 -0600

    heroku fix

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require("path");
 const api = require("./routes/index");
 const uuid = require("./helpers/uuid");
 const { clog } = require("./middleware/clog");
-const favicon = require("express-favicon");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(clog);
-
-app.use(favicon(__dirname + "/public/favicon.png"));
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
