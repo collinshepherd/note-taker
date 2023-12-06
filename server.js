515f593f9c4e5cad2fb432406b9bf8b761596703
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2023-12-05 22:21:43 -0600

    Final proofreading and commments

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+// Adding all required modules to run the server
 const express = require("express");
 const path = require("path");
 const api = require("./routes/index");
 const uuid = require("./helpers/uuid");
 const { clog } = require("./middleware/clog");
 
+// Defining port to run on heroku and locally
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware to make the console log more readable
 app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
@@ -29,6 +32,7 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// GET Route for anything that is not one of the ones listed above and it will route the user back to the home page.
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
